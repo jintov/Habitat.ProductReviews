be8cc3a004902c986c0041f700e275882b750bb0
kmuralidhar <kmurali007@gmail.com>
2016-01-30 18:24:41 +0530

    Added VideoReview Model

---
+++ src/Feature/VideoReviews/code/Models/VideoReviews.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.VideoReviews.Models
+{
+    public class VideoReviews
+    {
+    }
+}

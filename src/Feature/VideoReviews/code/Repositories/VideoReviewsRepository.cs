885d4354fd7e82b38316c5811523481f1c6789a1
Manohar5itagi <manohar.itagi@gmail.com>
2016-01-30 16:51:24 +0530

    Added video repository functionality...

--- src/Feature/VideoReviews/code/Repositories/VideoReviewsRepository.cs
+++ src/Feature/VideoReviews/code/Repositories/VideoReviewsRepository.cs
@@ -1,10 +1,40 @@
 ﻿namespace Sitecore.Feature.VideoReviews.Repositories
 {
-  using System.Web.Security;
-  using Sitecore;
-  using Sitecore.Diagnostics;
+    using System;
+    using System.Web.Security;
+    using Configuration;
+    using Data;
+    using Data.Items;
+    using Reviews;
+    using Sitecore;
+    using Sitecore.Diagnostics;
+    using Sitecore.Foundation.SitecoreExtensions.Extensions;
 
     public class VideoReviewsRepository : IVideoReviewsRepository
     {
+        public Item ContextItem { get; set; }
+
+        public VideoReviewsRepository(Item contextItem)
+        {
+            if (contextItem == null)
+            {
+                throw new ArgumentNullException(nameof(contextItem));
+            }
+            this.ContextItem = contextItem;
+        }
+
+
+        public Item GetGlobalSetting()
+        {
+            //TODO: replace with actual settings path
+            string globalSettingsPath = "";
+            return this.ContextItem.Database.GetItem(globalSettingsPath);
+        }
+
+        public string GetSku()
+        {
+            //TODO: replace with actual Sku field id
+            return this.ContextItem.FieldHasValue(ID.NewID) ? ContextItem.GetString(ID.NewID) : string.Empty;
+        }
     }
 }

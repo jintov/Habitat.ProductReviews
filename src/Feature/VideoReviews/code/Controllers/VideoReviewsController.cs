29d40c70dbacaaf52cb5a7b75c422a00671e2f8b
kmuralidhar <kmurali007@gmail.com>
2016-01-30 17:47:29 +0530

    Controller flow to repo and services added

--- src/Feature/VideoReviews/code/Controllers/VideoReviewsController.cs
+++ src/Feature/VideoReviews/code/Controllers/VideoReviewsController.cs
@@ -1,23 +1,34 @@
 ﻿namespace Sitecore.Feature.VideoReviews.Controllers
 {
-  using System;
-  using System.Web.Mvc;
-  using System.Web.Security;
-  using Sitecore;
-  using Sitecore.Diagnostics;
-  using Sitecore.Feature.VideoReviews.Repositories;
+    using System;
+    using System.Web.Mvc;
+    using System.Web.Security;
+    using Data.Items;
+    using Services;
+    using Sitecore;
+    using Sitecore.Diagnostics;
+    using Sitecore.Feature.VideoReviews.Repositories;
 
-  public class VideoReviewsController : Controller
-  {
-    private readonly IVideoReviewsRepository videoReviewsRepository;
-
-    public VideoReviewsController() : this(new VideoReviewsRepository(Context.Item))
+    public class VideoReviewsController : Controller
     {
-    }
+        private readonly IVideoReviewsService videoReviewsService;
+        private readonly IVideoReviewsRepository videoReviewsRepository;
 
-    public VideoReviewsController(IVideoReviewsRepository videoReviewsRepository)
-    {
-      this.videoReviewsRepository = videoReviewsRepository;
+        public VideoReviewsController() : this(new VideoReviewsRepository(Context.Item))
+        {
+            Item globalSetting = this.videoReviewsRepository.GetGlobalSetting();
+            string skuId = this.videoReviewsRepository.GetSku();
+            if (globalSetting != null && !string.IsNullOrWhiteSpace(skuId))
+            {
+                this.videoReviewsService.GetReviews(globalSetting, sku);
+            }
+
+        }
+
+        public VideoReviewsController(IVideoReviewsRepository videoReviewsRepository, IVideoReviewsService videoReviewsService)
+        {
+            this.videoReviewsRepository = videoReviewsRepository;
+            this.videoReviewsService = videoReviewsService;
+        }
     }
-  }
 }

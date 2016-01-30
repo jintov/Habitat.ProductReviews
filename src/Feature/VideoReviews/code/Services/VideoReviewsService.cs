1455dff8911bbfb33d85ac86e958f8af8e4a8af2
kmuralidhar <kmurali007@gmail.com>
2016-01-30 18:23:15 +0530

    Added get reviews code

--- src/Feature/VideoReviews/code/Services/VideoReviewsService.cs
+++ src/Feature/VideoReviews/code/Services/VideoReviewsService.cs
@@ -1,11 +1,49 @@
 ﻿namespace Sitecore.Feature.VideoReviews.Services
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Xml.Linq;
+    using Helpers;
+    using Reviews;
     using Sitecore.Data.Items;
+    using Sitecore.Foundation.SitecoreExtensions.Extensions;
 
     public class VideoReviewsService : IVideoReviewsService
     {
-        public virtual void GetReviews(Item settings, string sku)
+
+        //TODO:pick value from configuration
+        private static readonly TimeSpan APITimeout = new TimeSpan(0, 0, 10);
+        public virtual void GetReviews(Item expoTvsettings, string sku)
         {
+            if (expoTvsettings != null && !string.IsNullOrWhiteSpace(sku))
+            {
+                string expoTvUrl = expoTvsettings.FieldHasValue(Templates.VideoReviewsSettings.Fields.DetailedReviewURL) ?
+                      expoTvsettings.GetString(Templates.VideoReviewsSettings.Fields.DetailedReviewURL) + "/" + sku : string.Empty;
+                if (!string.IsNullOrWhiteSpace(expoTvUrl))
+                {
+                    NameValueCollection headers = GetHeadersForRequest(expoTvsettings);
+                    string expoTvResponse = ExpoTvExternalProviderClient.GetDataFromExpoTvProvider(expoTvUrl, headers).Result;
+                    XDocument expoResponseDoc = XDocument.Parse(expoTvResponse);
+                    if (expoResponseDoc != null && expoResponseDoc.Root != null &&
+                        expoResponseDoc.Root.Element("reviews") != null)
+                    {
+                        IEnumerable<XElement> reviewItems = expoResponseDoc.Root.Elements("review_item");
+                        ////get the request info sections
+                        //videoResponse = GetRequestInformation(doc.Root.Element("request_information"), videoReviewResponse);
+                        ////get the reviews
+                        //videoReviewResponse = GetReviews(reviewItems, videoReviewResponse);
+                    }
+                }
+
+            }
         }
-    }
-}
+        private NameValueCollection GetHeadersForRequest(Item expoTvsettings)
+        {
+            NameValueCollection headers = new NameValueCollection();
+            headers.Add("X-Expo_API_version", (expoTvsettings.FieldHasValue(Templates.VideoReviewsSettings.Fields.ClientId) ?
+                expoTvsettings.GetString("ApiVersion") : "2.0"));
+            headers.Add("X-Expo_API_client_id", (expoTvsettings.FieldHasValue(Templates.VideoReviewsSettings.Fields.ClientId) ?
+                expoTvsettings.GetString(Templates.VideoReviewsSettings.Fields.ClientId) : string.Empty));
+            return headers;
+        }

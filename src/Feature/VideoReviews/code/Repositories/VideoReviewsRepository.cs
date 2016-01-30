b36123d6bedc47f131c101f3b689ab479dd5d803
kmuralidhar <kmurali007@gmail.com>
2016-01-30 21:36:42 +0530

    Repository changes for getting global settings

--- src/Feature/VideoReviews/code/Repositories/VideoReviewsRepository.cs
+++ src/Feature/VideoReviews/code/Repositories/VideoReviewsRepository.cs
@@ -1,6 +1,7 @@
 ﻿namespace Sitecore.Feature.VideoReviews.Repositories
 {
     using System;
+    using System.Linq;
     using System.Web.Security;
     using Configuration;
     using Data;
@@ -9,30 +10,40 @@
     using Sitecore;
     using Sitecore.Diagnostics;
     using Sitecore.Foundation.SitecoreExtensions.Extensions;
-
+    using Sites;
     public class VideoReviewsRepository : IVideoReviewsRepository
     {
         public Item ContextItem { get; set; }
+        public SiteContext CurrentSite { get; set; }
 
-        public VideoReviewsRepository(Item contextItem)
+        public VideoReviewsRepository(Item contextItem, SiteContext currentSite)
         {
-            if (contextItem == null)
+            if (contextItem == null) 
             {
                 throw new ArgumentNullException(nameof(contextItem));
             }
+            if (currentSite == null)
+            {
+                throw new ArgumentNullException(nameof(currentSite));
+            }
             this.ContextItem = contextItem;
+            this.CurrentSite = currentSite;
         }
 
 
         public Item GetGlobalSetting()
-        {
-            //TODO: replace with actual settings path
-            string globalSettingsPath = "{F1BB8AA8-F01E-43A1-8FBF-BC17BA6CF859}";
-            return this.ContextItem.Database.GetItem(globalSettingsPath);
+        {   
+            Item currentItem = this.ContextItem.Database.GetItem(CurrentSite.StartPath);
+            if (currentItem != null)
+            {
+               return currentItem.Axes.GetDescendants().Where(item => item.TemplateID == ID.Parse("{15FD06FF-FB4D-484C-AE2B-F2F63EBA3263}"))
+                    .FirstOrDefault();
+            }
+            return null;
         }
 
         public string GetSku()
-        {
+        {            
             //TODO: replace with actual Sku field id
             return this.ContextItem.FieldHasValue(Templates.HasVideoReviews.Fields.VideoReviewSKU) ? ContextItem.GetString(Templates.HasVideoReviews.Fields.VideoReviewSKU) : string.Empty;
         }

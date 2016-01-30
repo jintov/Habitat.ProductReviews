ffb5468ba83af26b960be2e1eaabe53f94c0163f
Manohar5itagi <manohar.itagi@gmail.com>
2016-01-30 17:00:04 +0530

    Added video reviews functionality

---
+++ src/Feature/VideoReviews/code/Helpers/ExpoTvExternalProviderClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Sitecore.Feature.VideoReviews.Helpers
+{
+    public static class ExpoTvExternalProviderClient
+    {
+        public static async Task<string> GetDataFromExpoTvProvider(string url, NameValueCollection headers)
+        {
+
+            string expoTvData = null;
+            HttpResponseMessage expoTvResponse = null;
+
+            using (HttpClient client = new HttpClient())
+            {
+                foreach (string headerkey in headers.AllKeys)
+                {                    
+                    if (headers != null && !string.IsNullOrWhiteSpace(headers[headerkey]))
+                    {                        
+                        client.DefaultRequestHeaders.Add(headerkey, headers[headerkey]);
+                    }
+                }
+
+                expoTvResponse = await client.GetAsync(url).ConfigureAwait(false);
+
+                expoTvResponse.EnsureSuccessStatusCode();
+
+                expoTvData = await expoTvResponse.Content.ReadAsStringAsync();
+            }
+
+            return expoTvData;
+        }
+    }
+}
--- src/Feature/VideoReviews/code/Repositories/IVideoReviewsRepository.cs
+++ src/Feature/VideoReviews/code/Repositories/IVideoReviewsRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace Sitecore.Feature.VideoReviews.Repositories
+﻿using Sitecore.Data.Items;
+
+namespace Sitecore.Feature.VideoReviews.Repositories
 {
     public interface IVideoReviewsRepository
     {
+        /// <summary>
+        /// Gets the global settings from sitecore
+        /// </summary>
+        /// <returns>return the settings data back</returns>
+        Item GetGlobalSetting();
+
+        /// <summary>
+        /// gets the sku value from the 
+        /// </summary>
+        /// <param name="currentItem"></param>
+        /// <returns></returns>
+        string GetSku();
     }
 }
